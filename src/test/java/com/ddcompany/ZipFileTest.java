02538192e5f83c483558d4ed84ad4adc8f09aeb2
DaymianDark <nightkonn@gmail.com>
2022-01-17 16:08:37 +0300

    ZipFileTest finished, cosmetic

--- src/test/java/com/ddcompany/ZipFileTest.java
+++ src/test/java/com/ddcompany/ZipFileTest.java
@@ -1,25 +1,52 @@
 package com.ddcompany;
 
-import com.ddcompany.withDescritpion.FileParsingTest;
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-public class ZipFileTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    private ClassLoader cl = FileParsingTest.class.getClassLoader();
+public class ZipFileTest {
 
     @Test
     void zipFileTest() throws Exception {
 
         ZipFile zipFile = new ZipFile("src/test/resources/zip_sample.zip");
+        ZipEntry zipEntry;
 
+        zipEntry = zipFile.getEntry("csv_sample.csv");
+        try (InputStream stream = zipFile.getInputStream(zipEntry)) {
+            CSVReader reader = new CSVReader(new InputStreamReader(stream));
+            List<String[]> list = reader.readAll();
+            assertThat(list).hasSize(3).contains(
+                    new String[] {"author", "j.r.r.tolkien"},
+                    new String[] {"book","the lord of the rings"},
+                    new String[] {"volume 1", "the fellowship of the ring"}
+            );
+        }
 
-        ZipEntry zipCsvFile = zipFile.getEntry("csv_sample.csv");
+        zipEntry = zipFile.getEntry("xls_sample.xlsx");
+        try (InputStream stream = zipFile.getInputStream(zipEntry)) {
+            XLS parsedXLS = new XLS(stream);
+            assertThat(parsedXLS.excel
+                    .getSheetAt(0)
+                    .getRow(4)
+                    .getCell(1)
+                    .getStringCellValue())
+                    .isEqualTo("Kathleen");
+        }
 
+        zipEntry = zipFile.getEntry("pdf_sample.pdf");
+        try (InputStream stream = zipFile.getInputStream(zipEntry)) {
+            PDF parsedPDF = new PDF(stream);
+            assertThat(parsedPDF.title).contains("JUnit 5 User Guide");
+        }
     }
 }
